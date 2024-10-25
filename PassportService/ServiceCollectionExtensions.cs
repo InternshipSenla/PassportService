25005ad50bc94e1eda0b4d14001bb8ff3a9337fa
Evgeniy <eve-21111@yandex.ru>
2024-10-25 15:59:07 +0300

    Added docker-compose and automatic migration

--- PassportService/ServiceCollectionExtensions.cs
+++ PassportService/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using PassportService.Infrastructure;
 using PassportService.Services;
+using System.Data;
 
 public static class ServiceCollectionExtensions
 {
@@ -13,7 +14,18 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IPassportRepository, PassportRepository>();
         services.AddScoped<ICsvPassportLoaderService, CsvPassportLoaderService>();
         services.AddHostedService<PassportUpdateService>();
-
+        ApplyMigrations(services);
         return services;
+    }
+
+    private static void ApplyMigrations(IServiceCollection services)
+    { 
+        var serviceProvider = services.BuildServiceProvider();
+
+        using(var scope = serviceProvider.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<PassportDbContext>();      
+            dbContext.Database.Migrate();       
+        }
     }
 }
